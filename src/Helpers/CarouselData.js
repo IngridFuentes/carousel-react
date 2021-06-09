b10338e85fd6bb67c690fa92f622b787f1cffe8b
Ingrid04 <ingridfmelgar@gmail.com>
2021-06-09 17:23:24 -0400

    carousel project react

---
+++ src/Helpers/CarouselData.js
@@ -0,0 +1,15 @@
+import pic1 from '../images/pic1.jpg';
+import pic2 from '../images/pic2.jpg';
+import pic3 from '../images/pic3.jpg';
+
+export const images = [
+    {title: 'buildings', 
+    subtitle: "This is somewhere",
+    img: pic1},
+    {title: 'drink', 
+    subtitle: "This is a drink",
+    img: pic2},
+    {title: 'NY', 
+    subtitle: "This is the Empire State building",
+    img: pic3}
+];
